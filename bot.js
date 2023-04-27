c3fb14feb9db8275a5d66820154e290d9219ce8d
Ivan Labiak <71697480+ILabiak@users.noreply.github.com>
2023-04-27 18:12:25 +0300

    made delay to work correctly with telegram api +logging

--- bot.js
+++ bot.js
@@ -1,13 +1,16 @@
 "use strict";
 
 require("dotenv").config();
+const fs = require("fs");
 const Telegraf = require("telegraf");
 const Extra = require("telegraf/extra");
 const Markup = require("telegraf/markup");
 const session = require("telegraf/session");
 
-const bot = new Telegraf(process.env.BOT_TOKEN);
+const start = 62;
+const end = 634;
 
+const bot = new Telegraf(process.env.BOT_TOKEN);
 
 bot.start((ctx) => ctx.reply("start"));
 
@@ -16,16 +19,33 @@ bot.catch((err) => {
   console.log(err);
 });
 
-bot.command('forward', async (ctx) => {
-    for(let i = process.env.START; i <= process.env.END; i++){
-        try{
-            ctx.telegram.copyMessage(process.env.DESTINATION, process.env.SOURCE, i)
-        }catch(err){
-            console.log(err)
-        }
+bot.command("forward", async (ctx) => {
+  ctx.reply("started copying");
+  for (let i = start; i <= end; i++) {
+    // console.log(i);
+    try {
+      let id = await ctx.telegram.copyMessage(
+        process.env.DESTINATION,
+        process.env.SOURCE,
+        i
+      );
+      console.log(id);
+      fs.appendFileSync(
+        "./log.txt",
+        `Message ${i} copied successfully, message id: ${id?.message_id}\n`
+      );
+    } catch (err) {
+      console.log(err);
+      fs.appendFileSync(
+        "./log.txt",
+        `Error while copying message with id ${i}, ${err.message}\n`
+      );
     }
-})
-
+    //Also note that your bot will not be able to send more than 20 messages per minute to the same group.
+    await new Promise((resolve) => setTimeout(resolve, 3300));
+  }
+  ctx.reply("ended copying");
+});
 
 bot.launch();
 
